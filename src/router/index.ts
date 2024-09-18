6038bb2e40c05cc37708f9f138b1642f7b141f1a
tomiputra <tomi.mandala@privyid.tech>
2024-09-18 11:23:32 +0700

    feat: added route / for view Forms

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import FormView from '@/views/FormsView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'FormView',
-      component: () => import('../views/FormsView.vue')
+      component: FormView
     }
   ]
 })
